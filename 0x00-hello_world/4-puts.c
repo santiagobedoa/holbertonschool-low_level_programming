a7df13c2b2a7865c332b69a948a8bf08cd441755
Santiago <stgbedoya@gmail.com>
2022-02-10 18:22:19 -0500

    Thu Feb 10 18:22:19 -05 2022

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * function that prints "Programming is like building a multilingual puzzle
+ * prints using puts instead printf
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
