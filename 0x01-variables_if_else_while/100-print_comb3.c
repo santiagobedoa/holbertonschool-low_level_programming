2521e7aea0e22dfdf23660247266441e2f482171
Santiago <stgbedoya@gmail.com>
2022-02-11 17:17:59 -0500

    Fri Feb 11 17:17:59 -05 2022

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,15 @@ int main(void)
 	int b;
 
 	for (a = '0'; a <= '9'; a++)
+	{
 		for (b = '1'; b <= '9'; b++)
+		{
 			putchar(a);
 			putchar(b);
 			putchar(',');
 			putchar(' ');
+		}
+	}
 	return (0);
 }
 
