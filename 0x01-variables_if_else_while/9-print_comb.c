07dc0dac7a8c3d97f3642e51d1a7b49193e14e3f
Santiago <stgbedoya@gmail.com>
2022-02-11 16:33:36 -0500

    Fri Feb 11 16:33:36 -05 2022

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+		if (a != 9)
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
+
