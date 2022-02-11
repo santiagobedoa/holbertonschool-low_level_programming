e783ad3a3ca3623dd01b13300c4f195b98ff6eff
Santiago <stgbedoya@gmail.com>
2022-02-11 17:57:47 -0500

    Fri Feb 11 17:57:47 -05 2022

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,17 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-	    for (b = a + 1; b <= 9; b++)
-	    {
-	        printf("%d%d, ", a, b);
-	    }
+		for (b = a + 1; b <= 9; b++)
+		{
+			if (a == 8 && b == 9)
+			{
+				printf("%d%d", a, b);
+			}
+			else
+			{
+				printf("%d%d, ", a, b);
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
