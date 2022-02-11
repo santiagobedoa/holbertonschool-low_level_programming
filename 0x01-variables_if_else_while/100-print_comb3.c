ee0d221fa53904afd3eba992c059fe1890b26f90
Santiago <stgbedoya@gmail.com>
2022-02-11 17:51:38 -0500

    Fri Feb 11 17:51:38 -05 2022

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,14 @@
 int main(void)
 {
 	int a;
-	int b = 1;
+	int b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (;b <= '9'; b++)
-		{
-			putchar(a);
-			putchar(b);
-			putchar(',');
-			putchar(' ');
-		}
-		b++;
+	    for (b = a + 1; b <= 9; b++)
+	    {
+	        printf("%d%d, ", a, b);
+	    }
 	}
 	putchar('\n');
 	return (0);
