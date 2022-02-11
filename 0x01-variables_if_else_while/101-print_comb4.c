f005b6dccc121cb152afb4b7270cb24454fc0b11
Santiago <stgbedoya@gmail.com>
2022-02-11 18:11:14 -0500

    Fri Feb 11 18:11:14 -05 2022

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				if (a == 7 && b == 8 && c == 9)
+                		{
+                    			printf("%d%d%d", a, b, c);
+                		}
+                		else
+                		{
+                    			printf("%d%d%d, ", a, b, c);
+                		}
+		    	}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
