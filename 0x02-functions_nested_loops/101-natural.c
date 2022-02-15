6d1f8394a252a1c21f15dbb93372885b0e2cd52c
Santiago <santiagobedoa@gmail.com>
2022-02-15 18:18:12 -0500

    Tue Feb 15 18:18:12 -05 2022

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: int
+ */
+
+int main(void)
+{
+	int weighted_sum;
+	int counter;
+
+	for (counter = 1; counter <= 1023; counter++)
+	{
+		if ((counter % 3) == 0 || (counter % 5) == 0)
+		{
+			weighted_sum = weighted_sum + counter;
+		}
+	}
+	printf("%d", weighted_sum);
+	putchar('\n');
+}
+
