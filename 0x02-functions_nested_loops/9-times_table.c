f6ef8c7fbc722ad3ace617fa434746a180b1b60a
Santiago <santiagobedoa@gmail.com>
2022-02-15 15:05:48 -0500

    Tue Feb 15 15:05:48 -05 2022

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,18 +12,15 @@ void times_table(void)
 	int c;
 	int counter;
 
-	for (counter = 0; counter <= 8; counter++)
-	{
-		for (a = 0; a <= 9; a++)
-        	{
-                	for (b = 0; b <= 9; b++)
-                	{
-                        	c = a * b;
-                        	printf("%d, ", c);
-                	}
-                	putchar('\n');
-        	}
+	for (a = 0; a <= 9; a++)
+        {
+                for (b = 0; b <= 9; b++)
+                {
+                        c = a * b;
+                        printf("%d, ", c);
+                }
+                putchar('\n');
+        }
 	
-	}
 }
 
