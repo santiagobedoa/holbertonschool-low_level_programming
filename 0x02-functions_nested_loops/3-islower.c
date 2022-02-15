7388f6ff9458b6f0511419ddfd90d87f94eb2110
Santiago <santiagobedoa@gmail.com>
2022-02-15 10:57:10 -0500

    Tue Feb 15 10:57:09 -05 2022

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - init
+ * _islower - c is a latter
  * Return: 1 if c is lower case, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if ('a' <= c && c <= 'z') {
+	if ('a' <= c && c <= 'z')
 		return (1);
-	} else {
+	else
 		return (0);
-	}
 }
 
