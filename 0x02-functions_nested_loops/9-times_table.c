b623a0fc7cec9cd204e0d1512af7cb188530638f
Santiago <santiagobedoa@gmail.com>
2022-02-15 15:31:55 -0500

    Tue Feb 15 15:31:55 -05 2022

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,11 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c == 0)
+			if (a == 0)
+			{
+				printf("%d,  ", c);
+			}	
+			else if (c == 0)
 			{
 				printf("%d,", c);
 			}
