54162aa5d166389d28c2b08b831c9695aff6243b
Santiago <santiagobedoa@gmail.com>
2022-02-15 17:23:12 -0500

    Tue Feb 15 17:23:12 -05 2022

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,6 +15,7 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
+				break;
 			}
 			else
 			{
@@ -32,6 +33,7 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
+				break;
 			}
 			else
 			{
