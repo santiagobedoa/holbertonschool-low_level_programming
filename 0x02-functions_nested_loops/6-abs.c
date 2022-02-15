553b541a033f0d35dfdf1c11227e2f7ed93712ba
Santiago <stgbedoya@gmail.com>
2022-02-15 18:42:19 +0000

    iPad changes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,7 @@
 
 /**
  * _abs - computes the absolute value of an integer
+ * @n: integer to be processed
  * Return: absolute number
  */
 
@@ -9,10 +10,13 @@ int _abs(int n)
 {
 	int b;
 
-	if (n < 0) {
+	if (n < 0)
+	{
 		b = n * -1;
 		return (b);
-	} else {
+	}
+	else
+	{
 		return (n);
 	}
 }
