fde732dd8880c1fab5fefc7a4b000d93adf3ca3f
Santiago <santiagobedoa@gmail.com>
2022-02-15 11:34:21 -0500

    Tue Feb 15 11:34:21 -05 2022

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,9 @@
 
 int _isalpha(int c)
 {
-	if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+	if ('a' <= c && c <= 'z')
+		return (1);
+	else if ('A' <= c && c <= 'Z')
 		return (1);
 	else
 		return (0);
