62241873adcefe827a3dbc3420292c66f0f14f92
Santiago <santiagobedoa@gmail.com>
2022-02-15 15:45:34 -0500

    Tue Feb 15 15:45:34 -05 2022

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,10 +17,11 @@ void times_table(void)
 		{
 			c = a * b;
 			_putchar(c + '0');
-			putchar(", ");
+			_putchar(",");
+			_putchar(" ");
 			if (c <= 9)
 			{
-				putchar(" ");
+				_putchar(" ");
 			}
 		}
 		putchar('\n');
