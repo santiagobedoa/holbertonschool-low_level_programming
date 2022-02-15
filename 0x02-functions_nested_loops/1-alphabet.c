2c96e685db277b39d53789e5036b5261ab7c72bb
Santiago <santiagobedoa@gmail.com>
2022-02-15 10:29:12 -0500

    Tue Feb 15 10:29:12 -05 2022

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,22 +5,15 @@
  * Return: void
  */
 
-void print_alphabet(void);
-
 int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-void print_alphabet(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
 	{
-		putchar(a);
+		_putchar(a);
 	}
-	putchar('\n');
+	_putchar('\n');
+	return (0);
 }
 
