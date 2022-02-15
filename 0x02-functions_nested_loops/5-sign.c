e18a9e7740db9ab9dba5467aec6fb3a450dfbe24
Santiago <stgbedoya@gmail.com>
2022-02-15 17:49:22 +0000

    iPad changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - init
- * Return: 1 if greater than 0, 0 otherwise
+ * print_sign - prints the sign of a number
+ * @n: the number to be checked
+ * Return: 1 and prints + if n is greater than zero. 0 and prints 0 if n is zero. -1 and prints - if n is less than zero.
  */
 
 int print_sign(int n)
