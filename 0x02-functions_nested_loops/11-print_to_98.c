3926b2366b21393b9dd748eac9ed39916a589355
Santiago <santiagobedoa@gmail.com>
2022-02-15 17:28:16 -0500

    Tue Feb 15 17:28:16 -05 2022

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,15 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
+			printf("%d", n);
 			if (n == 98)
 			{
-				printf("%d", n);
 				break;
 			}
 			else
 			{
 				putchar(',');
 				putchar(' ');
-				printf("%d", n);
 				n++;
 			}
 		}
@@ -30,16 +29,15 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
+			printf("%d", n);
 			if (n == 98)
 			{
-				printf("%d", n);
 				break;
 			}
 			else
 			{
 				putchar(',');
 				putchar(' ');
-				printf("%d", n);
 				n--;
 			}
 		}
