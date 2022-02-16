fbfb7ca13eb0ccf65e706e0cd33b423c472b453a
Santiago <santiagobedoa@gmail.com>
2022-02-15 20:41:11 -0500

    Tue Feb 15 20:41:11 -05 2022

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: 0
+ */
+
+int main(void)
+{
+	long num1 = 0;
+	long num2 = 1;
+	long r;
+	long weighet_sum;
+
+	while (r <= 4000000)
+	{
+		r = num1, num2;
+		num1 = num2;
+		num2 = r
+		if ((r % 2) == 0)
+		{
+			weighted_sum = weighted_sum + r;
+		}
+	}
+	printf("%lld", weighted_sum);
+	putchar('\n');
+}
+
