5520c1ab4832fb2fc84beac03c1ca2c336cf5af4
Santiago <santiagobedoa@gmail.com>
2022-02-15 19:09:20 -0500

    Tue Feb 15 19:09:20 -05 2022

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+	int r;
+	int counter;
+
+	for (counter = 0; counter <= 49; counter++)
+	{
+		if (counter == 49)
+		{
+			printf("%d", r);
+			break;
+		}
+		else
+		{
+			r = num1 + num2;
+			printf("%d, ", r);
+			num1 = num2;
+			num2 = r
+		}
+	}
+}
+
