240cfd5a6271db7fdf659c2533b737e742869261
Santiago <santiagobedoa@gmail.com>
2022-02-17 09:24:54 -0500

    Thu Feb 17 09:24:54 -05 2022

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,21 @@ void more_numbers(void)
 	char a;
 	char b;
 
-	for (a = '0'; a < '10'; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = '0'; b <= '14'; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			putchar(b);
+			if (b <= 9)
+			{
+				putchar(b + '0');
+			}
+			else
+			{
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+			}
 		}
 		putchar('\n');
 	}
-	putchar('\n');
 }
 
