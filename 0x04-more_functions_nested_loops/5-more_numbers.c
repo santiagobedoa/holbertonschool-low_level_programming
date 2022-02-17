e9d60945799ebb8423bba41ae54efa504b3c6013
Santiago <santiagobedoa@gmail.com>
2022-02-17 09:11:43 -0500

    Thu Feb 17 09:11:43 -05 2022

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,22 @@
 
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14 followed by a new line
+ *
+ * Return: void
  */
 
 void more_numbers(void)
 {
 	int a;
-	int b;
+	char b;
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (b = '0'; b <= '14'; b++)
 		{
-			_putchar(b + '0');
+			putchar(b);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 	putchar('\n');
 }
