092ee87df852f0e4a636923b4ded432e3b2c303f
Santiago <santiagobedoa@gmail.com>
2022-02-17 08:38:30 -0500

    Thu Feb 17 08:38:30 -05 2022

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,21 +2,23 @@
 
 /**
  * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
+ *
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
-	int a;
+	char a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		if (a == 2 || a == 4)
+		if (a == '2' || a == '4')
 		{
 			continue;
 		}
 		else
 		{
-			_putchar(a + '0');
+			_putchar(a);
 		}
 	}
 	_putchar('\n');
