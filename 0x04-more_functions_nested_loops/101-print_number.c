4d090f0947744c20c4c3a7566e8cc1b8f3122db9
Santiago <santiagobedoa@gmail.com>
2022-02-17 22:08:24 -0500

    Thu Feb 17 22:08:24 -05 2022

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		num = -n;
 	}
 	else
@@ -23,6 +23,6 @@ void print_number(int n)
 	{
 		print_number(num / 10);
 	}
-	putchar((num % 10) + '0');
+	_putchar((num % 10) + '0');
 }
 
