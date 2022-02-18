401a1cc04025b72aaf1ce208255dfb16d728758c
Santiago <santiagobedoa@gmail.com>
2022-02-17 21:39:38 -0500

    Thu Feb 17 21:39:38 -05 2022

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print an int
+ * @n: int to be printed
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num / 10)
+	{
+		print_number(num/10);
+	}
+	_putchar((num % 10) + '0');
+}
+
