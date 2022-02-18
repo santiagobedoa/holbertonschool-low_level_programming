21704e2a5d82a75f648f194633ac2baf060d4088
Santiago <santiagobedoa@gmail.com>
2022-02-17 20:44:05 -0500

    Thu Feb 17 20:44:05 -05 2022

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - find largest factor of 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long n, i, r;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			r = n / i;
+		}
+	}
+	printf("%lu\n", r);
+	return (0);
+}
+
