cedb83278755f374cecb27d155d663adff59782e
Santiago <santiagobedoa@gmail.com>
2022-02-21 13:01:30 -0500

    Mon Feb 21 13:01:30 -05 2022

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a str followed by a new line to stdout
+ * @str: str to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+}
+
