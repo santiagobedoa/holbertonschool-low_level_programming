719c81e28278f80f6849b9e1c7b54b17edf92ccb
Santiago <santiagobedoa@gmail.com>
2022-02-21 14:50:49 -0500

    Mon Feb 21 14:50:49 -05 2022

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a str
+ * @s: str to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char reversed[];
+	int len;
+	int i = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		*(reversed + i) = s[len];
+		i++;
+	}
+	*s = reversed;
+}
+
