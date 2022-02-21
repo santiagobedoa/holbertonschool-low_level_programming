f89f5947d6972ab1d3970af9a117991a6d8cd345
Santiago <santiagobedoa@gmail.com>
2022-02-21 12:32:43 -0500

    Mon Feb 21 12:32:43 -05 2022

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: string to be processed
+ * Return: lenght of s
+ */
+
+int _strlen(char *s);
+{
+	return (sizeof(s));
+}
+
