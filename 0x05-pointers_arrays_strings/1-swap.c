49b28ad9871ee4b259614c1d0331bab43fa5c9c8
Santiago <santiagobedoa@gmail.com>
2022-02-21 12:26:52 -0500

    Mon Feb 21 12:26:52 -05 2022

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two integers
+ * @a: value one
+ * @b: value two
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	int a_original = *a;
+	int b_original = *b;
+
+	*a = b_original;
+	*b = a_original;
+}
+
