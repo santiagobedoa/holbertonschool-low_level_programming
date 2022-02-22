748df76b22941cf1d8b34cb2c86ad74f8743332a
Santiago <santiagobedoa@gmail.com>
2022-02-21 22:06:49 -0500

    Mon Feb 21 22:06:49 -05 2022

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,9 +21,12 @@ int _atoi(char *s)
 		sign = -1;
 		i++;
 	}
-	for (; s[i] != 0; i++)
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-		r = r * 10 + s[i] - '0';
+		for (; s[i] != 0; i++)
+		{
+			r = r * 10 + s[i] - '0';
+		}
 	}
 	return sign * r;
 }
