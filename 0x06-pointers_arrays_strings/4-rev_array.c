39d7d3891acdc947c898ca961267498d20e32ab1
Santiago <santiagobedoa@gmail.com>
2022-02-24 09:28:44 -0500

    Thu Feb 24 09:28:44 -05 2022

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse a content of an array of integers
+ * @a: array to be reversed
+ * @n: number of elements of the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int aux[n];
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		aux[n - 1 - i] = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = aux[i];
+	}
+}
+
