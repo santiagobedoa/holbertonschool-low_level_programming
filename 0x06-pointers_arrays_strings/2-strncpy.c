410034c3e57cdedf8692cec27233f53501018b10
Santiago <santiagobedoa@gmail.com>
2022-02-24 15:08:50 -0500

    Thu Feb 24 15:08:50 -05 2022

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,25 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int u = 0;
 	int i = 0;
-	int j = 0;
+	int j;
 
-	while (i < n)
+	while (dest[i] != '\0')
 	{
-		dest[j] = src[i];
 		i++;
+	}
+	while (src[u] != '\0')
+	{
+		u++;
+	}
+	for (j = 0; (j <= u) && (j < n); j++)
+	{
+		dest[j] = src[j];
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
 		j++;
 	}
 	return (dest);
