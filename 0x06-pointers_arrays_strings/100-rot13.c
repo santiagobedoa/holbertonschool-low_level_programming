7b72279eb5bb4c6a9690d82a82255b477c547294
Santiago <santiagobedoa@gmail.com>
2022-02-24 18:06:27 -0500

    Thu Feb 24 18:06:27 -05 2022

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,27 +8,19 @@
 
 char *rot13(char *s)
 {
-	int i = 0;
+	int i, j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'n')
+		for (j = 0; j < 10; j++)
 		{
-			*(s + i) += 13;
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+			}
 		}
-		else if (*(s + i) >= 'A' && *(s + i) <= 'N')
-		{
-			*(s + i) += 13;
-		}
-		else if (*(s + i) >= 'n' && *(s + i) <= 'z')
-		{
-			*(s + i) -= 13;
-		}
-		else if (*(s + i) >= 'N' && *(s + i) <= 'Z')
-		{
-			*(s + i) -= 13;
-		}
-		i++;
 	}
 	return (s);
 }
