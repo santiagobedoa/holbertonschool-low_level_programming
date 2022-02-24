e54b1a690836d3b09036be537e56f3ab33fb2260
Santiago <santiagobedoa@gmail.com>
2022-02-24 17:15:00 -0500

    Thu Feb 24 17:15:00 -05 2022

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,22 @@
  * Return: encoded string
  */
 
-char *leet(char *n)
+char *leet(char *s)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
+			if (s[i] == s1[j])
 			{
-				n[i] = s2[j];
+				s[i] = s2[j];
 			}
 		}
 	}
-	return (n);
+	return (s);
 }
+
