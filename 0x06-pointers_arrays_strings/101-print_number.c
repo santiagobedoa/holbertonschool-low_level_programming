6f78fd62023665389625dfe5ecb490917718e714
Santiago <santiagobedoa@gmail.com>
2022-02-25 08:28:01 -0500

    Fri Feb 25 08:28:01 -05 2022

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include "mian.h"
+#include "main.h"
 
 /**
  * print_number - prints an integer
@@ -8,29 +8,15 @@
 
 void print_number(int n)
 {
-	int aux;
-	int num = n;
-	int i = 0;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n
+		n = -n;
 	}
-	while (n)
+	if (n / 10)
 	{
-		while (n > 0)
-		{
-			n = n / 10;
-			i++;
-		}
-		_putchar(n);
-		while (i != 1)
-		{
-			n = n * 10;
-			i--;
-		}
-		n = num - n;
+		print_number(n / 10);
 	}
+	_putchar(n % 10 + '0');
 }
 
