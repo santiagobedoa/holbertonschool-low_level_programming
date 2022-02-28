2dd8803cf5ab8bd62ca42acc1cdc2ee7129a19f3
Santiago <santiagobedoa@gmail.com>
2022-02-28 15:22:31 -0500

    Mon Feb 28 15:22:31 -05 2022

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: main str to be scanned
+ * @needle: small str to be searched
+ * Return: pointer to the first ocurrence or null if not present
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			return (&haystack[i]);
+		}
+	}
+	return (NULL);
+}
+
