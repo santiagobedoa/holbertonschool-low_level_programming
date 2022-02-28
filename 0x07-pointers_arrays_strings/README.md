8fdb560f6fbca75e7cdeb79d4f0019d1dbf9ee2f
Santiago <santiagobedoa@gmail.com>
2022-02-28 11:32:47 -0500

    Mon Feb 28 11:32:47 -05 2022

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Solutiones for the project 0x07-pointers_arrays_strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN
+#define _MAIN
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
