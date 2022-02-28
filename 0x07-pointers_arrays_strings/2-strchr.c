6e46bd81130d7b4888ae7f16c7b4794b585351c4
Santiago <santiagobedoa@gmail.com>
2022-02-28 12:11:35 -0500

    Mon Feb 28 12:11:35 -05 2022

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a char in a str
+ * @s: str to be scanned
+ * @c: char to be searched in str
+ * Return: pointer to the first occurrence of char c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	char match = s[0];
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (match == c)
+		{
+			break;
+		}
+		else
+		{
+			i++;
+			match = *(s + i);
+		}
+	}
+	return (&s[i]);
+}
+
