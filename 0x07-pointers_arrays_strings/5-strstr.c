524547fb688dc7c1cfdb12a2537816e88e53ac03
Santiago <santiagobedoa@gmail.com>
2022-02-28 16:22:06 -0500

    Mon Feb 28 16:22:06 -05 2022

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,3 +21,13 @@ char *_strstr(char *haystack, char *needle)
 	return (NULL);
 }
 
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "llo";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: board
+ * Return: void
+ */
+
+void print_cheesboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
+
