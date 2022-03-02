2cffc1fca126f90a5568adb6ee9c4b926aa64661
Santiago <santiagobedoa@gmail.com>
2022-03-02 16:59:10 -0500

    Wed Mar  2 16:59:10 -05 2022

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * find_sqrt - find sqrt of a natural number
+ * @t: target
+ * @i: powered number
+ * Return: sqrt root of target
+ */
+
+int find_sqrt(int t, int i)
+{
+	if (i * i > t)
+	{
+		return (-1);
+	}
+	else if (i * i == t)
+	{
+		return (i);
+	}
+	return (find_sqrt(t, i + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: return natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqrt(n, 1))
+}
+
