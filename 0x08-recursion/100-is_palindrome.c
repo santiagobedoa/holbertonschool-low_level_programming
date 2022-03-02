cfeb1089351c3b0c25b586e73676d0af542ca1ea
Santiago <santiagobedoa@gmail.com>
2022-03-02 18:52:14 -0500

    Wed Mar  2 18:52:14 -05 2022

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * compare_chars - checks if two chars are equal
+ * @l: char 1
+ * @r: char 2
+ * Return: 1 if equal, 0 otherwise
+ */
+
+int compare_chars(char *s, int i, int j)
+{
+	if (i == j)
+	{
+		return (1);
+	}
+	if (s[i] != s[j])
+	{
+		return (0);
+	}
+	if (i < j + 1)
+	{
+		return compare_chars(s, i + 1, j - 1);
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a str is palindrome
+ * @s: str to be checked
+ * Return: 1 if str is palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (len == 0)
+	{
+		return (1);
+	}
+	return compare_chars(s, 0, len - 1);
+}
+
