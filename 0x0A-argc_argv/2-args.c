05e1811ae92be91913bf666cafde07b255a66dca
Santiago <santiagobedoa@gmail.com>
2022-03-04 14:05:04 -0500

    Fri Mar  4 14:05:04 -05 2022

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: arguments vector
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i];
+	}
+	return (0);
+}
+
