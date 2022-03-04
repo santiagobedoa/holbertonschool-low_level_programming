4d69e7ca80a77da65e811db3c68acc20d768589f
Santiago <santiagobedoa@gmail.com>
2022-03-04 12:22:33 -0500

    Fri Mar  4 12:22:33 -05 2022

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints its name
+ * @argc: number of argumts
+ * @argv: array of arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
 
