3e79132e61a1dd81bfb37b03e7e9c23cb5134b0e
Santiago <santiagobedoa@gmail.com>
2022-03-04 14:35:05 -0500

    Fri Mar  4 14:35:05 -05 2022

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positives numbers
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 0)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+			{
+			 sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
+
