723571b6f4ac30614304e7144ac5706034632b53
Santiago <santiagobedoa@gmail.com>
2022-03-04 13:51:03 -0500

    Fri Mar  4 13:51:03 -05 2022

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: number of arguments
+ * @argv: array that contains the arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
+
