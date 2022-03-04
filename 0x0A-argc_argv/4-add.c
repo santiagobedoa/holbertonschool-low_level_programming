d5fb7ef75b8b629bea66eec32b97a478d6cba78f
Santiago <santiagobedoa@gmail.com>
2022-03-04 14:56:43 -0500

    Fri Mar  4 14:56:43 -05 2022

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,25 +13,18 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 
 	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (!atoi(argv[i]))
 		{
-			if (atoi(argv[i]) && atoi(argv[i]) >= 0)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("%s\n", "Error");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
+
 	return (0);
 }
-
