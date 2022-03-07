b1338f1b84e5ac842a5cb00b93b57c03ca3a3705
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-07 13:17:42 -0500

    Mon Mar  7 13:17:42 -05 2022

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars initialized with specified char
+ * @size: size of the array
+ * @c: specified char
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+	i = 0;
+	while (i <= size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	if (arr)
+	{
+		return (arr);
+	}
+	return (NULL);
+}
+
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif
