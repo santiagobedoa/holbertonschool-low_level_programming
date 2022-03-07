75bc2d9e54b8a9bce3d0ec84b61192f7d99f1ec8
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-07 15:15:54 -0500

    Mon Mar  7 15:15:54 -05 2022

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,18 +22,24 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	i = 0;
-	while (s1[i] != '\0')
+	if (s1)
 	{
-		final_str[i] = s1[i];
-		i++;
+		i = 0;
+		while (s1[i] != '\0')
+		{
+			final_str[i] = s1[i];
+			i++;
+		}
 	}
-	j = 0;
-	while (s2[j] != '\0')
+	if (s2)
 	{
-		final_str[i] = s2[j];
-		i++;
-		j++;
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			final_str[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
 	return (final_str);
 }
