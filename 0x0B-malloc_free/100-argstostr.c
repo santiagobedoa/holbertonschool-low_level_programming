729fa8ad0faf143b8bc6b11557acd7b906e7942e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-08 11:08:29 -0500

    Tue Mar  8 11:08:29 -05 2022

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,14 +19,14 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
-	for (; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		for (; av[i][j]; j++)
+		for (j = 0; av[i][j]; j++)
 		{
 			size++;
 		}
+		size++;
 	}
-	size += ac;
 	arr = malloc(sizeof(char) * size + 1);
 	if (!arr)
 	{
