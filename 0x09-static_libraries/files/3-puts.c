fc028163bd4f99a73d80a469d58f1e479670e0e9
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-09 11:36:40 -0500

    Wed Mar  9 11:36:40 -05 2022

--- 0x09-static_libraries/files/3-puts.c
+++ 0x09-static_libraries/files/3-puts.c
@@ -7,8 +7,13 @@
  * Return: void
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
-	printf("%s\n", str);
-}
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
