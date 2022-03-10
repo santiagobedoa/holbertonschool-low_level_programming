b2eed834e70fd8f5f2b9c97c60147991f9b0190e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-10 16:49:40 -0500

    Thu Mar 10 16:49:40 -05 2022

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			a[i] = s2[j++];
 		}
-	}	
+	}
 	a[i] = '\0';
 	return (a);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memroy for an array
+ * @nmemb: number of block to be allocated
+ * @size: size of each block
+ * Return: pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(nmemb * size);
+	if (!arr)
+	{
+		return (NULL);
+	}
+	for (; i < nmemb * size; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
+
