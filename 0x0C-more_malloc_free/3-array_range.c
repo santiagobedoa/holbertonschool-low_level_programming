9558cafc6392f27488f0aa5aae06564f33e49dc0
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-10 18:36:22 -0500

    Thu Mar 10 18:36:22 -05 2022

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: inferior limit
+ * @max: superior limit
+ * Return: pointer to the array
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(int) * max - min + 1);
+	if (!arr)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= max; i++)
+	{
+		arr[i] = i;
+	}
+	return (arr);
+}
+
