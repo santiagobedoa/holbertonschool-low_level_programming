60c0bde9ca7099b6709c26c452a4e4a4864ff558
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-11 12:25:09 -0500

    Fri Mar 11 12:25:09 -05 2022

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,14 +16,14 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(*arr) * max - min + 1);
+	arr = malloc(sizeof(int) * max - min + 1);
 	if (!arr)
 	{
 		return (NULL);
 	}
-	for (i = min; i <= max; i++)
+	for (i = 0; i < (max - min) && min <= max; i++, min++)
 	{
-		arr[i] = i;
+		arr[i] = min;
 	}
 	return (arr);
 }
