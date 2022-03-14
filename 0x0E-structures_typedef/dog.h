b1abd113daffbb1a274642055d31addecb4fb443
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-14 18:03:28 -0500

    Mon Mar 14 18:03:28 -05 2022

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,10 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
