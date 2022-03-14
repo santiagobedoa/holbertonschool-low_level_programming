0e5d46ed7f866b4a9ca5fb2196fd6111a55c7c71
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-14 16:58:38 -0500

    Mon Mar 14 16:58:38 -05 2022

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: defines metadata about the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
