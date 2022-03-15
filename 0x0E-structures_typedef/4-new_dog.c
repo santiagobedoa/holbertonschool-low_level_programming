a894179deed8394ee9706aa4b766621849cb41a8
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-14 19:09:09 -0500

    Mon Mar 14 19:09:09 -05 2022

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	dog_t *dog
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+	{
+		return (NULL);
+	}
+	dog->name = malloc(strlen(name) * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < strlen(name); i++)
+	{
+		dog->name = name[i];
+	}
+	dog->age = age;
+	dog->owner = malloc(strlen(owner) * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < strlen(owner); i++)
+	{
+		dog->owner[i] = owner[i];
+	}
+
+	return (dog);
+}
