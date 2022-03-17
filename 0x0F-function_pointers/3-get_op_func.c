797651a5baee137ef95d38c62bb56d1e2a6a3873
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-17 16:05:13 -0500

    Thu Mar 17 16:05:13 -05 2022

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
