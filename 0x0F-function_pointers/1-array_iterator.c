ffde4e7748414a177b39d1b996a77ba7bd612141
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-17 11:32:12 -0500

    Thu Mar 17 11:32:12 -05 2022

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ * @array: array to be processed
+ * @size: size of the array
+ * @action: function to be applied
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array && action)
+	{
+		for(; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
