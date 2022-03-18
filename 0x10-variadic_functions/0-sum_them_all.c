8461866155ab96b935c849c3a50c651ba76d5792
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-18 14:20:34 -0500

    Fri Mar 18 14:20:34 -05 2022

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,15 +7,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	int sum = 0;
+	unsigned int i;
+	int sum;
 	va_list args;
 
 	va_start(args, n);
-	for (; i < n; i++)
+	for (i = 0, sum = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
-	va_end(args);
+	va_end(list);
 	return (sum);
 }
