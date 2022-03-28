e64408443a9f875fc9a0dce47c03a450d6b06298
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-28 15:35:17 -0500

    Mon Mar 28 15:35:17 -05 2022

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beggining of a linked list
+ * @head: previus node head
+ * @str: string of the new node
+ *
+ * Return: address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = (char *)str;
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
