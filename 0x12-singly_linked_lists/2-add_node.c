3865711996a03c722a144c4bb1734461aaf0ae0e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-29 10:28:01 -0500

    Tue Mar 29 10:28:01 -05 2022

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new->str = (char *)str;
+	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
