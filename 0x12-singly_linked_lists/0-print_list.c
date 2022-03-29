751b8d0144359756f963b79f0f46f4eeeac196cf
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-29 10:23:20 -0500

    Tue Mar 29 10:23:20 -05 2022

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,18 @@ size_t print_list(const list_t *h)
 {
 	int len_linked_list = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		h = h->next;
-		len_linked_list++;
-	}
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n")
+		}
 		len_linked_list++;
+		h = h->next;
 	}
 	return (len_linked_list);
 }
