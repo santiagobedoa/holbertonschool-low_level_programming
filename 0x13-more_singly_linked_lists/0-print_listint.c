45184bd9ba20f4cd6d706b21e5d76ce23c0336f7
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-31 10:39:05 -0500

    Thu Mar 31 10:39:05 -05 2022

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: head node
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int len_linked_list = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		len_linked_list++;
+		h = h->next;
+	}
+	return (len_linked_list);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# Solution for the project 0x13-more_singly_linked_lists
