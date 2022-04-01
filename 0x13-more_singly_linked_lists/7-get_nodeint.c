ecff6bacee75b20d19c6a117773d8a830e6606f5
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-01 09:22:00 -0500

    Fri Apr  1 09:22:00 -05 2022

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,17 +11,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 
-	if (head)
+	while (head != NULL)
 	{
-		while (counter < index)
-		{
-			head = head->next;
-			counter++;
-		}
-		if (head)
+		if (counter == index)
 		{
 			return (head);
 		}
+		head = head->next;
+		counter++;
 	}
 	return (NULL);
 }
