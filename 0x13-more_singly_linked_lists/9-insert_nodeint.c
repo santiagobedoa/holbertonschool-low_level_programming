a34664f11c09bdef351c6cdf438542886ed371d3
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-01 09:25:57 -0500

    Fri Apr  1 09:25:57 -05 2022

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,5 +41,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		counter++;
 		tmp = tmp->next;
 	}
+	if (counter < idx)
+	{
+		return (NULL);
+	}
 	return (new);
 }
