77bcd4b3461ae46f7a2478ef4be4b269833819ce
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-01 09:28:06 -0500

    Fri Apr  1 09:28:06 -05 2022

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp;
 	unsigned int counter = 0;
 
-	if (head == NULL) 
+	if (head == NULL)
 	{
 		return (NULL);
 	}
