6e3156b72f6864f1ba5c069546a5d6ca6d9b2c40
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-05 16:42:34 -0500

    Tue Apr  5 16:42:34 -05 2022

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,8 +7,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 /* PROTOTYPES */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
