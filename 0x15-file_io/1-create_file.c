e91a8d1eea485432f5feeaec9e53150cf9f5f198
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-05 16:49:53 -0500

    Tue Apr  5 16:49:53 -05 2022

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		len_content = strlen(text_content);
 		writen_bits = write(file, text_content, len_content);
-		if (writen_bits == error)
+		if (writen_bits == -1)
 		{
 			return (-1);
 		}
