0849cd7c3f728b9c82a68f099c1fb16238100723
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-05 18:35:39 -0500

    Tue Apr  5 18:35:39 -05 2022

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ void close_with_error(int fd)
 	error = close(fd);
 	if (error == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -39,13 +39,13 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (file_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while (file_from_read >= 1024)
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
 		file_from_read = read(file_from, buffer, 1024);
 		if (file_from_read == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close_with_error(file_from);
 			close_with_error(file_to);
 			exit(98);
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 		file_to_write = write(file_to, buffer, file_from_read);
 		if (file_to_write)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 
