d8dbec21ed35cd8f23d647056f44df9b4fa53893
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-12 09:32:20 -0500

    Thu May 12 09:32:20 -05 2022

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = *head;
-	*head = new;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		new->prev = NULL;
+		(*head)->prev = new;
+	}
 	return (new);
 }
