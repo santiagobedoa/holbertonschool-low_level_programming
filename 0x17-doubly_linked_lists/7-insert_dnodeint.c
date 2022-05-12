225b3a351ee327e90aa396608e11d6de68e584af
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-12 10:19:25 -0500

    Thu May 12 10:19:25 -05 2022

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,42 +10,38 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *tmp;
-	unsigned int counter = 0;
+	unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
 
-	if (h == NULL || *h == NULL)
+	if (new_node)
 	{
-		return (NULL);
-	}
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
-	if (idx == 0)
-	{
-		new->next = *h;
-		(*h)->prev = new;
-		*h = new;
-		return (*h);
-	}
-	tmp = *h;
-	while (counter < idx - 1 && tmp)
-	{
-		tmp = tmp->next;
-		counter++;
-	}
-	if (tmp)
-	{
-		if (tmp->next)
+		new_node->n = n;
+		if (idx == 0)
 		{
-			tmp->next->prev = new;
+			if (*h)
+			{
+				new_node->next = *h;
+				(*h)->prev = new_node;
+			}
+			*h = new_node;
+			return (new_node);
 		}
-		new->next = tmp->next;
-		new->prev = tmp;
-		tmp->next = new;
-		return (new);
+		while (i < idx - 1 && tmp_node)
+		{
+			tmp_node = tmp_node->next;
+			i++;
+		}
+		if (tmp_node)
+		{
+			if (tmp_node->next)
+			{
+				tmp_node->next->prev = new_node;
+			}
+			new_node->next = tmp_node->next;
+			new_node->prev = tmp_node;
+			tmp_node->next = new_node;
+			return (new_node);
+		}
 	}
 	return (NULL);
 }
