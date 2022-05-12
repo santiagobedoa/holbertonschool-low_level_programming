9cf1342329696158ad990fd464579ed6d92714e9
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-12 10:50:16 -0500

    Thu May 12 10:50:16 -05 2022

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,40 +9,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current_node;
-	unsigned int counter = 0;
+	unsigned int i = 0;
+	dlistint_t *bfr_node = *head, *tmp_node = *head;
 
-	if (head == NULL || *head == NULL)
+	if (index == 0 && *head)
 	{
-		return (-1);
-	}
-	if (index == 0)
-	{
-		if ((*head)->next == NULL)
+		if ((*head)->next)
 		{
-			*head = NULL;
-			return (1);
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+			free(tmp_node);
 		}
-		(*head)->prev = NULL;
-		*head = (*head)->next;
-		free(current_node);
+		else
+			*head = NULL;
 		return (1);
 	}
-	current_node = *head;
-	while (current_node)
+	while (i < index - 1 && bfr_node)
 	{
-		if (counter == index)
-		{
-			if (current_node->next)
-			{
-				current_node->next->prev = current_node;
-			}
-			current_node->next = current_node->next;
-			free(current_node);
-			return (1);
-		}
-		current_node = current_node->next;
-		counter++;
+		bfr_node = bfr_node->next;
+		i++;
+	}
+	if (bfr_node)
+	{
+		tmp_node = bfr_node->next;
+		if (tmp_node->next)
+			tmp_node->next->prev = bfr_node;
+		bfr_node->next = tmp_node->next;
+		free(tmp_node);
+		return (1);
 	}
 	return (-1);
 }
