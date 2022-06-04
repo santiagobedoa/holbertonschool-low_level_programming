1f3d6f3c161b6f91e244de44d5a271c8aa871e2b
Santiago Bedoya <santiagobedoa@gmail.com>
2022-06-04 13:11:05 -0500

    Sat Jun  4 13:11:05 -05 2022

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - returns the index of a key
+ * key: string to be mapped
+ * @size: size of the hash array
+ *
+ * Return: index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!size)
+	{
+		return (0);
+	}
+	return (hash_djb2(key) % size);
+}
