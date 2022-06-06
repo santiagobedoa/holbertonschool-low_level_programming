c533fa70eff10ea73de7e90a8b08ec5d5e457a75
Santiago Bedoya <santiagobedoa@gmail.com>
2022-06-06 10:22:25 -0500

    Mon Jun  6 10:22:25 -05 2022

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
-	hash_table = malloc(sizeof(hash_node_t));
 	if (hash_table == NULL)
 	{
+		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
+		fprintf(stderr, "Error: malloc failes\n");
 		free(hash_table);
 		return (NULL);
 	}
